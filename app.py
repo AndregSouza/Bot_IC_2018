0935cf7ca209f615062dab41734ccb368db8c81d
Andre Souza <andregsouzaweb@gmail.com>
2018-11-18 21:56:08 -0200

    Update app.py

--- app.py
+++ app.py
@@ -47,3 +47,8 @@ def my_form_post():
     iniciaConversa()
     return render_template('index.html', conversation=responses)
 app.run()
+
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port)
